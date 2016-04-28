2765e73430e5a9cbb6cecb30f568b6a30ccf9892
Glushchenko Alexandr <sparklone@gmail.com>
2016-04-28 20:36:42 +0300

    better explain difference between observeOn and subscribeOm

--- DescribeOperators.playground/Pages/subscribeOn.xcplaygroundpage/Contents.swift
+++ DescribeOperators.playground/Pages/subscribeOn.xcplaygroundpage/Contents.swift
@@ -2,21 +2,60 @@
 
 import Foundation
 import RxSwift
+import XCPlayground
+
+XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
 
 //: [Next switchLast](@next)
-example("without subscribeOn") {
-    let sequence = Observable<AnyObject>.of(1, 2, 3)
+example("without observeOn and subscribeOn") {
+    print("init thread: \(NSThread.currentThread())")
+    let sequence = Observable<Int>.create { observer in
+        print("observable thread: \(NSThread.currentThread())")
+        observer.on(.Next(1))
+        observer.on(.Next(2))
+        observer.on(.Next(3))
+        observer.on(.Completed)
+        return NopDisposable.instance
+    }
     sequence.subscribe { e in
-        print("\(NSThread.currentThread()) \(e)")
+        print("observer thread: \(NSThread.currentThread()) \(e)")
     }
 }
-
-example("with subscribeOn") {
-    let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
-    let sequence = Observable<AnyObject>.of(1, 2, 3)
-        .subscribeOn(ConcurrentDispatchQueueScheduler.init(queue: queue))
+example("with subscribeOn only") {
+    let queue1 = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+    print("init thread: \(NSThread.currentThread())")
+    let sequence = Observable<Int>.create { observer in
+        print("observable thread: \(NSThread.currentThread())")
+        observer.on(.Next(1))
+        observer.on(.Next(2))
+        observer.on(.Next(3))
+        observer.on(.Completed)
+        return NopDisposable.instance
+        }
+        .subscribeOn(SerialDispatchQueueScheduler(internalSerialQueueName: "queue1"))
     
     sequence.subscribe { e in
-        print("\(NSThread.currentThread()) \(e)")
+        print("observer thread: \(NSThread.currentThread()) \(e)")
+    }
+}
+sleep(1)
+example("with subscribeOn and observeOn") {
+    let queue1 = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+    let queue2 = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+    print("init thread: \(NSThread.currentThread())")
+    let sequence = Observable<Int>.create { observer in
+        print("observable thread: \(NSThread.currentThread())")
+        observer.on(.Next(1))
+        observer.on(.Next(2))
+        observer.on(.Next(3))
+        observer.on(.Completed)
+        return NopDisposable.instance
+        }
+        .subscribeOn(SerialDispatchQueueScheduler(internalSerialQueueName: "queue1"))
+        .observeOn(SerialDispatchQueueScheduler(internalSerialQueueName: "queue2"))
+    
+    
+    sequence.subscribe { e in
+        print("observer thread: \(NSThread.currentThread()) \(e)")
     }
 }
